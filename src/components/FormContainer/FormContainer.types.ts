42ca8ed8c28a931378f424fafa25a44271a973da
Eugene Tsalko <96073690+EugeneTsalko@users.noreply.github.com>
2022-11-15 21:46:19 +0300

    Delete src directory

--- src/components/FormContainer/FormContainer.types.ts
+++
@@ -1,9 +0,0 @@
-export interface IPersonProps {
-  firstName: string | undefined;
-  lastName: string | undefined;
-  birthDate: string | undefined;
-  location: string | undefined;
-  dataAgree?: boolean | undefined;
-  sex: string | undefined;
-  photo?: string | undefined;
-}
